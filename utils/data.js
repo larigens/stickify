b9da1666039bdc1364fccf2bde3c6fad849b85c3
larigens@gmail.com <larigens@gmail.com>
2023-03-10 14:29:17 -0500

    added helper file to update the user's thoughts arrays with the id generated by the db

--- utils/data.js
+++ utils/data.js
@@ -2,53 +2,33 @@ const userData = [
     {
         username: 'larigens',
         email: 'larigens@gmail.com',
-        // thoughts: ["640a58b6adfc7126a813f7c1", "640a58b6adfc7126a813f7c2"],
-        // friends: ["61b6c468f27f350a154ebdf6", "61b6c468f27f350a154ebdh8"],
-        _id: '640a58b6adfc7126a813f7c4'
     },
     {
         username: 'hpotter',
         email: 'hpotter@hogwarts.edu',
-        // thoughts: ["61b6c468f27f350a154ebde6"],
-        // friends: ["61b6c468f27f350a154ebdf6", "61b6c468f27f350a154ebdg7", "61b6c468f27f350a154ebdh8"],
-        _id: '61b6c468f27f350a154ebde5'
     },
     {
         username: 'hgranger',
         email: 'hgranger@hogwarts.edu',
-        // thoughts: ["61b6c468f27f350a154ebdf4", "61b6c468f27f350a154ebdf5"],
-        // friends: ["61b6c468f27f350a154ebde5", "61b6c468f27f350a154ebdg7", "61b6c468f27f350a154ebdi9"],
-        _id: '61b6c468f27f350a154ebdf6'
     },
     {
         username: 'rweasley',
         email: 'rweasley@hogwarts.edu',
-        // thoughts: ["61b6c468f27f350a154ebdg8", "61b6c468f27f350a154ebdg9"],
-        // friends: ["61b6c468f27f350a154ebde5", "61b6c468f27f350a154ebdi9"],
-        _id: '61b6c468f27f350a154ebdg7'
     },
     {
         username: 'sblack',
         email: 'sblack@outlook.com',
-        // thoughts: ["61b6c468f27f350a154ebdh9"],
-        // friends: ["61b6c468f27f350a154ebde5", "61b6c468f27f350a154ebdi9"],
-        _id: '61b6c468f27f350a154ebdh8'
     },
     {
         username: 'dobby',
         email: 'dobby@yahoo.com',
-        // thoughts: ["61b6c468f27f350a154ebdi4"],
-        // friends: ["61b6c468f27f350a154ebde5", "61b6c468f27f350a154ebdh8"],
-        _id: '61b6c468f27f350a154ebdi9'
     }
 ]
 
 const thoughtData = [
     {
-        _id: '640a58b6adfc7126a813f7c1',
         thoughtText: "Here's a cool thought...",
         username: "larigens",
-        createdAt: "3/9/2023, 5:22:51 PM",
         reactions: [
             {
                 reactionBody: "Agree!",
@@ -61,10 +41,8 @@ const thoughtData = [
         ]
     },
     {
-        _id: '640a58b6adfc7126a813f7c2',
         thoughtText: "I'm excited for the weekend!",
         username: "larigens",
-        createdAt: "3/9/2023, 5:30:51 PM",
         reactions: [
             {
                 reactionBody: "Me too!",
@@ -77,10 +55,8 @@ const thoughtData = [
         ]
     },
     {
-        _id: '61b6c468f27f350a154ebde6',
         thoughtText: "I almost learned a new trick today!",
         username: "hpotter",
-        createdAt: "3/9/2023, 3:22:05 PM",
         reactions: [
             {
                 reactionBody: "Call me!",
@@ -89,10 +65,8 @@ const thoughtData = [
         ]
     },
     {
-        _id: '61b6c468f27f350a154ebdf4',
         thoughtText: "Studying...",
         username: "hgranger",
-        createdAt: "3/8/2023, 7:22:51 PM",
         reactions: [
             {
                 reactionBody: "You're so smart!",
@@ -105,10 +79,8 @@ const thoughtData = [
         ]
     },
     {
-        _id: '61b6c468f27f350a154ebdf5',
         thoughtText: "It's leviosa, not leviosar!",
         username: "hgranger",
-        createdAt: "3/9/2023, 5:22:51 PM",
         reactions: [
             {
                 reactionBody: "Agree!",
@@ -129,10 +101,8 @@ const thoughtData = [
         ]
     },
     {
-        _id: '61b6c468f27f350a154ebdg8',
         thoughtText: "Percy Wouldn't Recognize A Joke If It Danced Naked In Front Of Him Wearing Dobby's Tea Cozy.",
         username: "rweasley",
-        createdAt: "3/9/2023, 1:12:10 PM",
         reactions: [
             {
                 reactionBody: "HAHAHAHAHAHAH!",
@@ -149,10 +119,8 @@ const thoughtData = [
         ]
     },
     {
-        _id: '61b6c468f27f350a154ebdg9',
         thoughtText: "Who's down to play some Wizard chess?",
         username: "rweasley",
-        createdAt: "3/9/2023, 6:22:11 PM",
         reactions: [
             {
                 reactionBody: "I'm down!",
@@ -165,10 +133,8 @@ const thoughtData = [
         ]
     },
     {
-        _id: '61b6c468f27f350a154ebdh9',
         thoughtText: "What's Life Without A Little Risk?",
         username: "sblack",
-        createdAt: "3/7/2023, 10:22:11 AM",
         reactions: [
             {
                 reactionBody: "Can't wait for our next adventure!",
@@ -181,10 +147,8 @@ const thoughtData = [
         ]
     },
     {
-        _id: '61b6c468f27f350a154ebdi4',
         thoughtText: "Dobby is freeeee",
         username: "dobby",
-        createdAt: "3/9/2023, 2:22:11 PM",
         reactions: [
             {
                 reactionBody: "We've all known this for a while",
